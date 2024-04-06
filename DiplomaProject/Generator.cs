40e308841fb3d52c93e0fec20b5b831631fcdf6a
Alexander <aleksandrrechica@gmail.com>
2024-04-06 23:00:18 +0300

    generator completed

--- DiplomaProject/Generator.cs
+++ DiplomaProject/Generator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace DiplomaProject
 {
@@ -18,12 +19,34 @@ namespace DiplomaProject
 
         public void GenerateEquentialsFiles(int equentialsCount)
         {
+            var xSet = _eGenerator.GenerateX(equentialsCount);
+            var ySet = _eGenerator.GenerateY(equentialsCount);
+            var zSet = _eGenerator.GenerateZ(equentialsCount);
 
+            using (FileStream fs = new FileStream(xRowsFileName, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize(fs, xSet);
+            }
+
+            using (FileStream fs = new FileStream(yRowsFileName, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize(fs, ySet);
+            }
+
+            using (FileStream fs = new FileStream(zRowsFileName, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize(fs, zSet);
+            }
         }
 
-        public void GenerateGammaFile(ulong key)
+        public void GenerateGammaFile(ulong key, int gammaCount)
         {
+            var gamma = _gGenerator.GenerateGamma(key, gammaCount);
 
+            using (FileStream fs = new FileStream(gammaFileName, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize(fs, gamma);
+            }
         }
 
     }
